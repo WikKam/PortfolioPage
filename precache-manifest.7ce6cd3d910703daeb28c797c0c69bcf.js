974cc19294a67a56369e13bb6fba7d89bed0039b
WikKam <kamykikamienie@gmail.com>
2020-07-12 02:27:11 +0200

    Updates

---
+++ precache-manifest.7ce6cd3d910703daeb28c797c0c69bcf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1d91f404a8d3a2d46f466d11de34cf",
+    "url": "/PortfolioPage/index.html"
+  },
+  {
+    "revision": "bbb1227f981dae0f9ef2",
+    "url": "/PortfolioPage/static/css/main.87722be6.chunk.css"
+  },
+  {
+    "revision": "3cdd1b247399ebcc582d",
+    "url": "/PortfolioPage/static/js/2.11d417c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PortfolioPage/static/js/2.11d417c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb1227f981dae0f9ef2",
+    "url": "/PortfolioPage/static/js/main.cd6edb49.chunk.js"
+  },
+  {
+    "revision": "4b667557206ae54141e2",
+    "url": "/PortfolioPage/static/js/runtime-main.10a74289.js"
+  },
+  {
+    "revision": "e3638831af5d4e002bef9b123b0422aa",
+    "url": "/PortfolioPage/static/media/WDAI.e3638831.jpg"
+  },
+  {
+    "revision": "2fabc6fecda1470bebf418e7839e8caf",
+    "url": "/PortfolioPage/static/media/autochess.2fabc6fe.jpg"
+  },
+  {
+    "revision": "be43c4097a700f7b48b75f08f392e922",
+    "url": "/PortfolioPage/static/media/coviddatatracker.be43c409.jpg"
+  },
+  {
+    "revision": "f74ff820ffaf4c2ca7238d0dca924ce2",
+    "url": "/PortfolioPage/static/media/demagogscrapper.f74ff820.jpg"
+  },
+  {
+    "revision": "c5752cc32fe94aea8dfd30edecbf425c",
+    "url": "/PortfolioPage/static/media/evolutionsimulator.c5752cc3.jpg"
+  },
+  {
+    "revision": "cd3fae09422a2275d699249f3c0983c8",
+    "url": "/PortfolioPage/static/media/jschess.cd3fae09.jpg"
+  }
+]);
